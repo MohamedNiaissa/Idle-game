4da9fcff8846bee2c5981a40a5f36d22936d73e4
admin <admin@MacBook-Pro-de-felix.local>
2022-10-05 15:20:19 +0200

    successful api call

--- src/stores/storage.js
+++ src/stores/storage.js
@@ -1,14 +1,30 @@
-import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
+import axios from 'axios'
 
-// "access_token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Ik1yTGVnZW5kIiwic3ViIjoxLCJyb2xlIjowLCJpYXQiOjE2NjQ5NjkzNzYsImV4cCI6MTY2NTU3NDE3Nn0.d1A3eWk92Tnj7tQhtdN0gvewrZNN2NmgEPttHQHaOlM"
 
-export const useStorageStore = defineStore('storage', () => {
-  const count = ref(0)
-  const doubleCount = computed(() => count.value * 2)
-  function increment() {
-    count.value++
-  }
+export default defineStore('storage',{
+  state: () => ({
+    coins: 0,
+    factories: [],
+    token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Ik1yTGVnZW5kIiwic3ViIjoxLCJyb2xlIjowLCJpYXQiOjE2NjQ5NjkzNzYsImV4cCI6MTY2NTU3NDE3Nn0.d1A3eWk92Tnj7tQhtdN0gvewrZNN2NmgEPttHQHaOlM"
+ }),
+  actions: {
+    // async fetchCoins() {
+    //     const { data } = await axios.get('://api.coingecko.com/api/v3/coins/list?include_platform=false')
+    //     this.coins = data;
+    //     console.log(this.coins);
+    // },
+    async fetchFactories() {
+        const dataFactories  = await axios.get('http://apigame.co/factories' , {
+           headers : {
+            "Authorization" : "Bearer " + this.token,
+            'Access-Control-Allow-Origin': '*',
+           }
+          })
 
-  return { count, doubleCount, increment }
+        this.factories = dataFactories;
+
+        console.log(this.factories);
+    }
+  },
 })
--- src/views/UsinesView.vue
+++ src/views/UsinesView.vue
@@ -1,5 +1,24 @@
 <template>
+
   <body>
-      <div>Usines :)</div>
+    <div>Usines :)</div>
+
+    <button @click="this.fetchFactories()"> hey </button>
   </body>
 </template>
+
+<script>
+import { mapActions } from 'pinia'
+import useStorageStore from "../stores/storage.js"
+
+export default {
+  methods: {
+    // printFactories() {
+    //   console.log(this.fetchFactories())
+      
+    // },
+    ...mapActions(useStorageStore, ['fetchFactories'])
+  }
+}
+
+</script>
