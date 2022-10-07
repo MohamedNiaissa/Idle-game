be1f2616738d2fe9dbf61100ca73b65af0de3085
Mohamed <mohameddrame907@gmail.com>
2022-10-07 09:33:03 +0200

    add little bit of style

--- src/views/MarketView.vue
+++ src/views/MarketView.vue
@@ -219,7 +219,7 @@ export default {
     }
     .market {
         display: flex;
-        justify-content: space-between;
+        justify-content: space-around;
         color: black;
     }
     .form {
